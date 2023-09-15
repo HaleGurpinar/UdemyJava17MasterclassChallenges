bdc66275bcf4aa6131b49f4d19baa4ea8fffc892
Hale <itshalenurgurpinar@gmail.com>
2023-09-16 00:07:37 +0300

    MasterChall--BillsBurger

--- src/BillsBurger_OOPMasterChallenge/Main.java
+++ src/BillsBurger_OOPMasterChallenge/Main.java
@@ -10,10 +10,30 @@ public class Main {
 //    Item mushroom= new Item("Topping","mushroom",7.0);
 //    mushroom.setSize("SMALL");
 //    mushroom.printItem();
+//-------------------------------------------------------------------------------------------------
+//        Burger burger= new Burger("regular ",10.0);
+//        burger.printItem();
+//        burger.addToppings("MUSHROOM","SALAMI","CHEESE");
+//        burger.printItem();
+//-------------------------------------------------------------------------------------------------//        MealOrder regularMeal1 = new MealOrder();
+//        regularMeal1.printItemizedList();
 
-        Burger burger= new Burger("regular ",10.0);
-        burger.printItem();
-        burger.addToppings("MUSHROOM","SALAMI","CHEESE");
-        burger.printItem();
+
+//        MealOrder regularMeal= new MealOrder();
+//        regularMeal.addBurgerToppings("BACON","CHEESE","MAYO");
+//        regularMeal.setDrinkSize("LARGE");
+//        regularMeal.printItemizedList();
+
+
+//        MealOrder secondMeal= new MealOrder("turkey ","7-up","chili");
+//        secondMeal.addBurgerToppings("LETTUCE","CHEESE","MAYO");
+//        secondMeal.setDrinkSize("SMALL");
+//        secondMeal.printItemizedList();
+
+        MealOrder deluxeMeal= new MealOrder("deluxe ","7-up","chili");
+       // deluxeMeal.addBurgerToppings("MUSHROOM","BACON","LETTUCE","CHEESE","MAYO");
+        deluxeMeal.addBurgerToppings("MUSHROOM","BACON","LETTUCE","CHEESE","MAYO");
+        deluxeMeal.setDrinkSize("SMALL");
+        deluxeMeal.printItemizedList();
     }
 }
