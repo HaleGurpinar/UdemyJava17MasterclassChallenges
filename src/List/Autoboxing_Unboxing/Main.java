88b30198fed89d5a38846636b1556a343776fe32
Hale <itshalenurgurpinar@gmail.com>
2023-09-24 23:58:25 +0300

    Autoboxing-Unb. Challenge

--- src/List/Autoboxing_Unboxing/Main.java
+++ src/List/Autoboxing_Unboxing/Main.java
@@ -15,5 +15,45 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Customer bob= new Customer("Bob S",1000.0);
+        System.out.println(bob);
+        Bank bank = new Bank("Chase");
+        bank.addNewCustomer("Jane A",500.0);
+        System.out.println(bank);
+    }
+}
+
+class Bank{
+    private String name;
+    private ArrayList<Customer> customers= new ArrayList<>(5000);
+
+    public Bank(String name){
+        this.name= name;
+    }
+
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "name='" + name + '\'' +
+                ", customers=" + customers +
+                '}';
+    }
+
+    private Customer getCustomer(String customerName){
+        for (var customer : customers){
+            if (customer.name().equalsIgnoreCase(customerName)){
+                return customer;
+            }
+        }
+        System.out.printf("Customer (%s) wasn't found %n",customerName);
+        return null;
+    }
+
+    public void addNewCustomer(String customerName, double initialDeposit){
+        if (getCustomer(customerName)==null){
+            Customer customer= new Customer(customerName,initialDeposit);
+            customers.add(customer);
+            System.out.println("New Customer added: "+ customer);
+        }
     }
 }
