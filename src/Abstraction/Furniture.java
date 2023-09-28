a679df7c32027aa04520a4edaffe2efa909b1c29
Hale <itshalenurgurpinar@gmail.com>
2023-09-28 23:52:38 +0300

    Abstraction Challenge

--- src/Abstraction/Furniture.java
+++ src/Abstraction/Furniture.java
@@ -1,14 +1,14 @@
 package Abstraction;
 
-public class ProductA extends ProductForSale{
+public class Furniture extends ProductForSale{
 
-    public ProductA(String type, double price, String description) {
+    public Furniture(String type, double price, String description) {
         super(type, price, description);
     }
 
     @Override
     public void showDetails() {
-        System.out.println("This "+ type+ "is a beautiful reproduction");
+        System.out.println("This "+ type+ " was manufactured in North Carolina");
         System.out.printf("The price of the piece is $%6.2f %n",price);
         System.out.println(description);
     }
--- src/Abstraction/OrderItem.java
+++
@@ -1,7 +0,0 @@
-package Abstraction;
-
-public record OrderItem() {
-    private static int qty;
-    static ProductForSale product;
-
-}
