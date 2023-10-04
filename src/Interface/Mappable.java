b6a702e12f02ddde79947fd9f7ab97a08131c67e
Hale <itshalenurgurpinar@gmail.com>
2023-10-04 19:29:14 +0300

    Interface Challenge

--- src/Interface/Mappable.java
+++ src/Interface/Mappable.java
@@ -1,4 +1,26 @@
 package Interface;
 
+enum Geometry{
+    LINE,POINT,POLYGON
+}
+enum Color{BLACK,BLUE,GREEN, WHITE,RED}
+enum PointMarkers{CIRCLE,DIAMOND,STAR,TRIANGLE}
+enum LineMarkers{DASHED,DOTTED,SOLID}
 public interface Mappable {
+
+    String JSON_PROPERTY= """
+           "properties": {%s} """;
+
+    static void mapIt(Mappable mappable){
+        System.out.println(JSON_PROPERTY.formatted(mappable.toJSON()));
+    }
+
+    default String toJSON(){
+        return """
+               "type": "%s", "label": "%s", "marker": "%s" """
+                .formatted(getShape(),getLabel(),getMarker());
+    }
+    String getLabel();
+    String getMarker();
+    Geometry getShape();
 }
