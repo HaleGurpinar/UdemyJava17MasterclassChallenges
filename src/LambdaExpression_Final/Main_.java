5deb7d65d74d47e9be4f102bd3135f98891c4c79
Hale <itshalenurgurpinar@gmail.com>
2023-10-26 11:55:10 +0300

    Lambda Expressions Method Challenge

--- src/LambdaExpression_Final/Main_.java
+++ src/LambdaExpression_Final/Main_.java
@@ -1,4 +1,56 @@
 package LambdaExpression_Final;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.function.UnaryOperator;
+
+//Method Reference Challenge
 public class Main_ {
+    private static Random random= new Random();
+    private record Person(String first){
+
+        public String last(String s){
+            return first +" "+ s.substring(0,s.indexOf(" "));
+        }
+    }
+    public static void main(String[] args) {
+
+        String[] names={"Anna","Bob","Cameron","Donald","Eva","Francis"};
+
+        Person tim= new Person("Tim");
+        List<UnaryOperator<String>> list= new ArrayList<>(List.of(
+                String::toUpperCase,
+                s->s += " " + getRandomChar('D','M')+ ".",
+                s->s += " "+ reverse(s,0,s.indexOf(" ")),
+                Main_::reverse,
+                String:: new,
+                s->new String(s),
+                String::valueOf,
+                tim::last,
+                (new Person("Mary")):: last));
+
+        applyChanges(names,list);
+    }
+
+    private  static void applyChanges(String[] names,
+                                      List<UnaryOperator<String >> stringFunctions){
+        List<String> backedArray = Arrays.asList(names);
+        for (var function : stringFunctions) {
+            backedArray.replaceAll(s->s.transform(function));
+            System.out.println(Arrays.toString(names));
+        }
+    }
+
+    private static char getRandomChar(char startChar, char endChar){
+        return (char) random.nextInt((int) startChar,(int) endChar+1);
+    }
+
+    private static String reverse(String s){
+        return reverse(s,0,s.length());
+    }
+    private static String reverse(String s, int start, int end){
+        return new StringBuilder(s.substring(start,end)).reverse().toString();
+    }
 }
