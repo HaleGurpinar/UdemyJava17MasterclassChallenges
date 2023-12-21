e3e6efd1fb1dc032d344fd5f214e94fa4dd38f1a
Hale <itshalenurgurpinar@gmail.com>
2023-12-21 17:41:50 +0300

    Collection Ch-

---
+++ src/Collection_CardGame/poker/GameController.java
@@ -0,0 +1,11 @@
+package Collection_CardGame.poker;
+//import  Collection_CardGame.poker.PokerGame;
+public class GameController {
+
+    public static void main(String[] args) {
+
+        PokerGame fiveCardDraw = new PokerGame(8, 5);
+        fiveCardDraw.startPlay();
+
+    }
+}
