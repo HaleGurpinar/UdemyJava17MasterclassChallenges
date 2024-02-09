84b71ab9563935f920bc858a07d79bfd707670c2
Hale <itshalenurgurpinar@gmail.com>
2024-02-09 20:16:00 +0300

    Challenge - Constructors_Initializers_PirateGame

--- src/Constructers_Initializers_PirateGame/Main.java
+++ src/Constructers_Initializers_PirateGame/Main.java
@@ -1,10 +1,25 @@
 package Constructers_Initializers_PirateGame;
 
+import Constructers_Initializers_PirateGame.pirate.Pirate;
+import Constructers_Initializers_PirateGame.pirate.Weapon;
+
 public class Main {
     public static void main(String[] args) {
-        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
+// Constructors Initializers Part -- Pirate Game
+//        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
+//
+//        int playerIndex = console.addPlayer();
+//        console.playGame(playerIndex);
 
-        int playerIndex = console.addPlayer();
-        console.playGame(playerIndex);
+        Weapon weapon = Weapon.getWeaponByChar('P');
+        System.out.println("weapon = " + weapon +
+                           ", hitPoints = " + weapon.getHitPoints() +
+                        ", minLevel = " + weapon.getMinLevel());
+
+        var list = Weapon.getWeaponsByLevel(1);
+        list.forEach(System.out::println);
+
+        Pirate lily = new Pirate("Lily");
+        System.out.println(lily);
     }
 }
